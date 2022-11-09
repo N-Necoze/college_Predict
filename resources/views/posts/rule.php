21d99b764db4a10d04da0e7910ae6e8c51655bf3
N-Necoze <n.nishidaneco@gmail.com>
2022-11-09 09:31:18 +0900

    save workspace 20221108

---
+++ resources/views/posts/rule.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Predict Numbers - rule</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <div>
+        <div class="filter">
+            <body>
+                <div class="title">
+                    <p>ナンバーズ３のルール</p>
+                </div>
+                <div class="boxp">
+                    <p class=text>ナンバーズ3は、000から999までの中から好きな3ケタの数字と申込タイプを選びます。</p>
+                    <p class=text>抽せんされた3ケタの数字と、その並び方によって当せん金額が決定します。</p>
+                    <p class=text>また、ナンバーズ3には、下2ケタだけを選ぶ「ミニ」という購入方法もあります。</p>
+                    <p class=text>ストレート当せんなら約10万円（理論値は約9万円）です。</p>
+                </div>
+                <div>
+                </div>
+            </body>
+        </div>
+    </div>
+</html>
